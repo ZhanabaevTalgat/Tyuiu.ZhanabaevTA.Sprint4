bcd7ca76cfdf94fd0fef8960ee4f9f120e8f6304
ZhanabaevTalgat <t.zhanabaeff@yandex.ru>
2024-10-23 14:54:37 +0500

    Task5

---
+++ Tyuiu.ZhanabaevTA.Sprint4.Task5.V12/Program.cs
@@ -0,0 +1,51 @@
+﻿using Tyuiu.ZhanabaevTA.Sprint4.Task5.V12.Lib;
+
+namespace Tyuiu.ZhanabaevTA.Sprint4.Task5.V12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Жанабаев Т.А | РППб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема:  Двумерные массивы (Генератор случайных чисел)                    *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #12                                                             *");
+            Console.WriteLine("* Выполнил: Жанабаев Талгат Асылбекович | РППб-24-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* случайными значениями в диапазоне от -6 до 4. Найти количество          *");
+            Console.WriteLine("* отрицательных элементов                                                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Random random = new Random();
+            int[,] ms = new int[5, 5];
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    ms[i, j] = random.Next(-6, 4);
+                    
+                    Console.Write("   " + ms[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+            int res = ds.Calculate(ms);
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
